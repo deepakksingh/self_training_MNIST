48e758ea715b676a6ca056a598dd24c93b6b1521
Deepak Kumar Singh <deepak.singh@research.iiit.ac.in>
2019-06-27 22:16:53 +0530

    added dataset and dataloader part

--- main.py
+++ main.py
@@ -1,12 +1,45 @@
 import argparse
 from logger import *
 from configReader import *
+import torch
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
 
 
 def runner(cfg, logger):
     '''contains code to train, validate and test on MNIST dataset'''
-    print(cfg)
+
+    logger.info("config content below")
     logger.info(cfg)
+
+    #specify transformations
+    transform = transforms.Compose(
+        [
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5),(0.5, 0.5, 0.5))
+        
+        ]
+    )
+
+    logger.info("loading MNIST dataset")
+    #get the dataset
+    train_set = datasets.MNIST('./data', train = True, download = True, transform = transform)
+    test_set = datasets.MNIST('./data', train = False, download = True, transform = transform)
+    logger.info("loaded MNIST dataset")
+
+    #prepare the data loader
+    logger.info("preparing data loader")
+    train_loader = torch.utils.data.DataLoader(train_set, batch_size = 100, shuffle = True, num_workers = 4)
+    test_loader = torch.utils.data.DataLoader(test_set, batch_size = 100, shuffle = True, num_workers = 4)
+    logger.info("prepared data loader")
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    processing_on = "cuda" if torch.cuda.is_available() else "cpu"
+    logger.info("processing on: " + processing_on)
+    
+    #validate
+
+    #save if needed
 
 if __name__ == "__main__":
     
